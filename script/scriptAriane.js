ffc54c325d657d5a8dacecd20a65c59d38314997
Moi9632 <134287533+Moi9632@users.noreply.github.com>
2025-06-10 16:05:59 +0200

    Update scriptAriane.js

--- script/scriptAriane.js
+++ script/scriptAriane.js
@@ -4,24 +4,31 @@ document.addEventListener('DOMContentLoaded', function () {
   const etapes = document.querySelectorAll('.etape');
   console.log("etapes : ", etapes);
 
-  window.addEventListener('scroll', () => {
-    let currentId = "";
+  if (sections.length > 0 && etapes.length > 0) {
+    window.addEventListener('scroll', () => {
+      let currentId = "";
 
-    sections.forEach(section => {
-      const rect = section.getBoundingClientRect();
-      if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-        currentId = section.id;
-      }
-    });
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+          currentId = section.id;
+        }
+      });
 
-    etapes.forEach(etape => {
-      if (etape.dataset.slide === currentId) {
-        etape.classList.add('active');
-        console.log(etape, etape.classList);
-      } else {
-        etape.classList.remove('active');
-        console.log(etape, etape.classList);
-      }
+      etapes.forEach(etape => {
+        if (etape.dataset.slide === currentId) {
+          etape.classList.add(`active-${currentId}`);
+          console.log(etape, etape.classList);
+        } else {
+          etape.classList.forEach(cls => {
+            if (cls.startsWith('active-')) {
+              etape.classList.remove(cls);
+            }
+          });
+          console.log(etape, etape.classList);
+        }
+      });
     });
-  });
+  }
 });
+
